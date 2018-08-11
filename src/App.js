ce3391fb682a3800dff00b99faede87d5a0e4b4f
yousuf451 <yousufshaikh92@gmail.com>
2018-08-11 17:25:58 +0500

    front page edit with a logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h1 className="text-center">This site is comming soon !!</h1>
+      <div>
+        <Header/>
       </div>
     );
   }
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+const Header = () => {
+    return(
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12 col-sm-6">
+                    <div className="pull-left">
+                        <div className="pull-left">
+                            <a href="" title="Market Place - Sell and Buy on Market Place"><img src="" alt="market place logo"/></a>
+                        </div>
+                        <span className="pull-right">Pakistan's Largest Marketplace</span>
+                    </div>
+                    <div className="pull-right">
+                        <h1>Right</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
