0b365ba6079d1cb81ef3d912abdf21ab2b477151
yousuf451 <yousufshaikh92@gmail.com>
2018-08-12 14:11:44 +0500

    logo and navigation left side edit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,31 @@
 import React from 'react'
+import logo from '../images/olx-logo.png'
 
 
 const Header = () => {
+    const logoStyle = {
+        width: '92px',
+        height: '92px'
+    }
+    const plm = {
+        fontSize: '18px',
+        fontWeight: 'bold',
+        position: 'absolute',
+        top: '34px',
+        left: '125px'
+    }
     return(
         <div className="container">
             <div className="row">
                 <div className="col-lg-12 col-sm-6">
                     <div className="pull-left">
                         <div className="pull-left">
-                            <a href="" title="Market Place - Sell and Buy on Market Place"><img src="" alt="market place logo"/></a>
+                            <a href="" title="Market Place - Sell and Buy on Market Place"><img src={logo} style={logoStyle} alt="market place logo"/></a>
                         </div>
-                        <span className="pull-right">Pakistan's Largest Marketplace</span>
+                        <span className="pull-right" style={plm}>Pakistan's Largest Marketplace</span> 
                     </div>
                     <div className="pull-right">
-                        <h1>Right</h1>
+                        
                     </div>
                 </div>
             </div>
