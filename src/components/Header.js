1166b50c3acf794076d629af69a98d312c835de5
yousuf451 <yousufshaikh92@gmail.com>
2018-08-12 15:17:06 +0500

    buttons add in home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,18 +14,24 @@ const Header = () => {
         top: '34px',
         left: '125px'
     }
+    const navbarBtnsMargin={
+        margin: '24px 0'
+    }
     return(
         <div className="container">
             <div className="row">
                 <div className="col-lg-12 col-sm-6">
                     <div className="pull-left">
                         <div className="pull-left">
-                            <a href="" title="Market Place - Sell and Buy on Market Place"><img src={logo} style={logoStyle} alt="market place logo"/></a>
+                            <a title="Market Place - Sell and Buy on Market Place"><img src={logo} style={logoStyle} alt="market place logo"/></a>
                         </div>
                         <span className="pull-right" style={plm}>Pakistan's Largest Marketplace</span> 
                     </div>
-                    <div className="pull-right">
-                        
+                    <div className="pull-right" style={navbarBtnsMargin}>
+                        <ul className="nav navbar-nav">
+                            <li className="nav-item"><button className="btn btn-default"><i class="fa fa-user"></i> My Account</button></li>
+                            <li className="nav-item"><button className="btn btn-warning">Submit an Ad</button></li>
+                        </ul>
                     </div>
                 </div>
             </div>
