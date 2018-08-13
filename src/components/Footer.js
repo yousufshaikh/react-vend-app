e3162fc1f13b275ae8aadab4e9806a7305103a57
yousuf451 <yousufshaikh92@gmail.com>
2018-08-13 15:14:12 +0500

    unorderlist settings and style

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,48 @@
 import React from 'react'
+import logo from '../images/olx-logo.png'
 
 const Footer = (props) => {
     const footerContainer ={
         borderTop: '2px solid #eee'
     }
+    const logoStyle = {
+        width: '92px',
+        height: '92px'
+    }
+    const containerWrapper = {
+        marginTop: '30px'
+    }
+    const unOrderList = {
+        listStyle: 'none'
+    }
     return(
         <div style={footerContainer}>
-            <div className="container">
+            <div className="container" style={containerWrapper}>
                 <div className="row">
                     <div className="col-lg-8">
                         <div className="pull-left">
-                            <h1>Heading One</h1>
+                            <img src={logo} style={logoStyle} alt="OLX.com.pk - OLX Pakistan"/>
                         </div>
-                        <div className="pull-right">
-                            <h1>Heading One</h1>
+                        <div className="pull-left">
+                            <ul className="unOrderList">
+                                <li>Local Map</li>
+                                <li>Popular Searches</li>
+                                <li>Archive</li>
+                                <li>Sitemap</li>
+                            </ul>
+                        </div>
+                        <div className="pull-left">
+                            <ul className="unOrderList">
+                                <li>Terms of Use</li>
+                                <li>Help & Contact Us</li>
+                            </ul>
+                        </div>
+                        <div className="pull-left">
+                            <ul className="unOrderList">
+                                <li>Who We Are</li>
+                                <li>Join OLX</li>
+                                <li>Happy OLXers</li>
+                            </ul>
                         </div>
                     </div>
                     <div className="col-lg-4">
