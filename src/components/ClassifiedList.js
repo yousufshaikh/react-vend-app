3b2472a014c45883109ec95a69c7de3647c3c72f
yousuf451 <yousufshaikh92@gmail.com>
2018-08-16 18:27:09 +0500

    category firls table row completed

--- src/components/ClassifiedList.js
+++ src/components/ClassifiedList.js
@@ -1,14 +1,66 @@
 import React from 'react'
 import '../styles/classified-list.css'
+import properties from '../images/properties.png'
+import cars from '../images/cars.png'
+import  electronics from '../images/electronics.png'
+import furniture from '../images/furniture.png'
 
 const ClassifiedList = () => {
     return(
-        <ul className="olx-category-list">
-            <li><a href=""><i class="fa fa-4x fa-home" aria-hidden="true"></i>Property for Sale</a></li>
-            <li><a href=""><i class="fa fa-4x fa-building" aria-hidden="true"></i>Property for Rent</a></li>
-            <li><a href=""><i class="fa fa-4x fa-car" aria-hidden="true"></i>Vehicles</a></li>
-            <li><a href=""><i class="fa fa-4x fa-motorcycle" aria-hidden="true"></i>Bikes</a></li>
-        </ul>
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <div>
+                            <a href="">
+                                <img src={properties} alt="properties"/>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <span>Properties</span>
+                            </a>
+                        </div>
+                    </td>
+                    <td>
+                        <div>
+                            <a href="">
+                                <img src={cars} alt="cars"/>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <span>Cars</span>
+                            </a>
+                        </div>
+                    </td>
+                    <td>
+                        <div>
+                            <a href="">
+                                <img src={electronics} alt="electronics"/>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <span> Electronics</span>
+                            </a>
+                        </div>
+                    </td>
+                    <td>
+                        <div>
+                            <a href="">
+                                <img src={furniture} alt="furniture"/>
+                            </a>
+                        </div>
+                        <div>
+                            <a href="">
+                                <span>Furniture</span>
+                            </a>
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
     );
 }
 
