61dce2d5277737d7a59ed2d4c2ced7c1ce5e4e17
yousuf451 <yousufshaikh92@gmail.com>
2018-09-03 01:57:15 +0500

    add posting page updated

--- src/components/AddPosting.js
+++ src/components/AddPosting.js
@@ -6,7 +6,7 @@ const AddPosting = (props) =>{
         <div className="container">
             <div className="row">
                 <div className="col-lg-12">
-                    <div className="post-container">
+                    <div className="post-ad-container">
                         <h3>Post Your Ad</h3>
                         <form>
                             <div className="form-group"></div>
@@ -36,26 +36,49 @@ const AddPosting = (props) =>{
                             </div>
                             <div className="image-margin">
                                 <div className="col-lg-4">
-                                    <div className="form-froup">
+                                    <div className="form-group">
                                         <label>Image 1</label>
                                         <input type="file" className="form-control"/>
                                     </div>
                                 </div>
                                 <div className="col-lg-4">
-                                    <div className="form-froup">
+                                    <div className="form-group">
                                         <label>Image 2</label>
                                         <input type="file" className="form-control"/>
                                     </div>
                                 </div>
                                 <div className="col-lg-4">
-                                    <div className="form-froup">
+                                    <div className="form-group">
                                         <label>Image 3</label>
                                         <input type="file" className="form-control"/>
                                     </div>
                                 </div>
                             </div>
-                            <hr/>
-                            
+                            <span className="text-danger"></span>
+                            <div className="divider"></div>
+                            <div className="form-group">
+                                <h4>Seller Information</h4>
+                            </div>
+                            <div className="form-group">
+                                <label>Seller Name</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Item to be sold in which city? *</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <label>Seller phone number</label>
+                                <input type="text" className="form-control"/>
+                            </div>
+                            <div className="form-group">
+                                <span className="text-muted">
+                                    <small>By clicking Submit you confirm that you have carefully read and understood all the facts, statements and conditions stated in the Terms of Use & Posting Rules of our website to which you unconditionally agree and accept as true and correct and constituting a binding agreement between us.</small>
+                                </span>
+                            </div>
+                            <div className="form-group">
+                                <button className="btn btn-primary btn-lg">Submit Your Ad</button>
+                            </div>
                         </form>
                     </div>
                 </div>
