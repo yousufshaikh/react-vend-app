91702910f2ab632d57a85e1ea3d699bb0046b8f9
yousuf451 <yousufshaikh92@gmail.com>
2018-09-05 13:44:55 +0500

    add bindings to register-user.js

--- src/components/RegisterUser.js
+++ src/components/RegisterUser.js
@@ -7,8 +7,21 @@ class RegisterUser extends Component{
         isLoading: true,
         token: '',
         signUpError: '',
-        signInError: ''
+        signInError: '',
+        signInEmail: '',
+        signInPassword: '',
+        signUpFirstName: '',
+        signUpLastName: '',
+        signUpEmail: '',
+        signUpPassword: ''
     }
+
+    onChangeTextBoxSignInEmail = this.onChangeTextBoxSignInEmail.bind(this)
+    onChangeTextBoxSignInPassword = this.onChangeTextBoxSignInPassword.bind(this)
+    onChangeTextBoxSignUpEmail = this.onChangeTextBoxSignUpEmail.bind(this)
+    onChangeTextBoxSignUpFirstName = this.onChangeTextBoxSignUpFirstName.bind(this)
+    onChangeTextBoxSignUpLastName = this.onChangeTextBoxSignUpLastName.bind(this)
+    onChangeTextBoxSignUpPassword = this.onChangeTextBoxSignUpPassword.bind(this)
 
     componentDidMount(){
         const token = getFromStorage('the_main_app');
@@ -35,6 +48,42 @@ class RegisterUser extends Component{
         }
     }
 
+    onChangeTextBoxSignInEmail(event){
+        this.setState({
+            signInEmail : event.target.value,
+        });
+    }
+
+    onChangeTextBoxSignInPassword(event){
+        this.setState({
+            signInPassword: event.target.value,
+        });
+    }
+
+    onChangeTextBoxSignUpFirstName(event){
+        this.setState({
+            signUpFirstName: event.target.value,
+        });
+    }
+
+    onChangeTextBoxSignUpLastName(event){
+        this.setState({
+            signUpLastName: event.target.value,
+        });
+    }
+
+    onChangeTextBoxSignUpEmail(event){
+        this.setState({
+            signUpEmail : event.target.value,
+        });
+    }
+
+    onChangeTextBoxSignUpPassword(event){
+        this.setState({
+            signUpPassword: event.target.value,
+        })
+    }
+
     render(){
         if (this.state.isLoading) {
             return(
@@ -48,21 +97,25 @@ class RegisterUser extends Component{
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="login-form-grid">
+                                {
+                                    (this.state.signInError) ? (
+                                        <p>{this.state.signInError}</p>
+                                    ) : (null)
+                                }
                                 <h2 className="login-style"><strong>Log In</strong></h2>
                                 <div className="login-box">
                                     <div className="login-form-inner">
-                                        
                                         <form>
                                             <div>
                                                 <label className="pull-left label-form">Your Email</label>
                                                     <div className="input-box-div">
-                                                        <input type="text" className="input-box" placeholder="Your Email"/>
+                                                        <input type="text" className="input-box" placeholder="Your Email" value={this.state.signInEmail} onChange={this.onChangeTextBoxSignInEmail}/>
                                                     </div>
                                             </div>
                                             <div>
                                                 <label className="pull-left label-form">Password</label>
                                                     <div className="input-box-div">
-                                                        <input type="password" className="input-box" placeholder="Password"/>
+                                                        <input type="password" className="input-box" placeholder="Password" value={this.state.signInPassword}/>
                                                     </div>
                                             </div>
                                             <div className="login-btn-div">
@@ -82,25 +135,25 @@ class RegisterUser extends Component{
                                             <div>
                                                 <label className="pull-left label-form">First Name</label>
                                                     <div className="input-box-div">
-                                                        <input type="text" className="input-box" placeholder="First Name"/>
+                                                        <input type="text" className="input-box" placeholder="First Name" value={this.state.signUpFirstName}/>
                                                     </div>
                                             </div>
                                             <div>
                                                 <label className="pull-left label-form">Last Name</label>
                                                     <div className="input-box-div">
-                                                        <input type="text" className="input-box" placeholder="Last Name"/>
+                                                        <input type="text" className="input-box" placeholder="Last Name" value={this.state.signUpLastName}/>
                                                     </div>
                                             </div>
                                             <div>
                                                 <label className="pull-left label-form">Email</label>
                                                     <div className="input-box-div">
-                                                        <input type="text" className="input-box" placeholder="Your Email"/>
+                                                        <input type="text" className="input-box" placeholder="Your Email" value={this.state.signUpEmail}/>
                                                     </div>
                                             </div>
                                             <div>
                                                 <label className="pull-left label-form">Password</label>
                                                     <div className="input-box-div">
-                                                        <input type="password" className="input-box" placeholder="Password"/>
+                                                        <input type="password" className="input-box" placeholder="Password" value={this.state.signUpPassword}/>
                                                     </div>
                                             </div>
                                             <div className="signup-btn-div">
