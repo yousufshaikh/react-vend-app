29145ff4727573b88fa9d3c4aab0525c308091d0
yousuf451 <yousufshaikh92@gmail.com>
2018-09-10 15:40:55 +0500

    view adds button and functionality added

--- src/components/ViewAds.js
+++ src/components/ViewAds.js
@@ -1,13 +1,54 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
+import 
+
+
 
 class ViewAds extends Component{
+
+    state = {
+        isLoading: true,
+        ads: []
+    }
+
+    componentDidMount(){
+        this.fetchData();
+    }
+    
+    fetchData(){
+        console.log("fetching Data function");
+
+        fetch('http://localhost:3001/adverts/ads')
+        .then(response => response.json())
+        .then(parsedJSON => parsedJSON.results.map(ad => (
+            {
+                title: `${ad.title}`
+            }
+        )))
+        .then(ads => this.setState({
+            ads,
+            isLoading: false
+        }))
+        console.log("josn", josn)
+        .catch(error => console.log("parsing fails", error))
+    }
+
     render(){
+        const {isLoading, ads} = this.state;
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
-                        <h1>This is a view ads page</h1>
+                        {
+                            !isLoading && ads.length > 0 ? ads.map(ad => {
+                                <ul className="list-group" key={ad.index}>
+                                    <li className="list-group-item">{ad.title}</li>
+                                </ul> 
+                            }) : null
+                        }
                     </div>
+                </div>
+                <div className="text-center" >
+                    <p>Ads are loading ....</p>
                 </div>
             </div>
         );
